335a1dfa5e95e8e05456080f46d8abfb02d2c0d0
simarbagga <simarbagga401@gmail.com>
2024-06-08 12:08:21 +0530

    update serverUrl

--- src/app/serverUrl.ts
+++ src/app/serverUrl.ts
@@ -1,1 +1,2 @@
-export const serverUrl = "http://localhost:8000";
+// export const serverUrl = "http://localhost:8000";
+export const serverUrl = "https://todo-list-backend-production-9212.up.railway.app";
